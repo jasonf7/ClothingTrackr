de7bc54143056b99efae873b779248f4d3b29370
jasonf7 <jason.z.fang@gmail.com>
2014-10-01 01:18:40 -0400

    improved UI

--- src/com/example/clothingtrackr/MainActivity.java
+++ src/com/example/clothingtrackr/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.clothingtrackr;
 
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+        
+        Tab tab = actionBar.newTab().setText(R.string.tab_what_to_wear);
+        tab.setTabListener(this);
+        actionBar.addTab(tab);
+        tab = actionBar.newTab().setText(R.string.tab_closet);
+        tab.setTabListener(this);
+        actionBar.addTab(tab);
+        tab = actionBar.newTab().setText(R.string.tab_settings);
+        tab.setTabListener(this);
+        actionBar.addTab(tab);
     }
 
 
@@ -28,9 +44,33 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sort) {
             return true;
+        }
+        else if(id == R.id.action_search) {
+        	return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
